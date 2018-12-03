da6c1649a0cc343b03b2d9ffd6feaf4fb36d680a
Charles Ribeiro <chp333@aol.com>
2018-12-03 16:43:27 -0500

    Uploading HW

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Burger with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Triple Burger with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Volcano Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mexian Burger');
